583021cc57b6425cf7bc552edd19de1d932fca13
Anton Berezhnoj <berejant@gmail.com>
2016-12-06 20:50:35 +0200

    Убрал ненужный use

--- app/User.php
+++ app/User.php
@@ -5,7 +5,6 @@ namespace Kneu\Petition;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 
