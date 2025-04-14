1673136714eefee989a7bff5b3b2ed4151be19f8
Anton Berezhnyi <berejant@gmail.com>
2025-04-14 15:12:51 +0300

    Update templates

--- config/petition.php
+++ config/petition.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'voting_days' => env('PETITION_VOTING_DAYS', 90) * 86400,
-    'votes_count_for_success' => env('PETITION_VOTES_COUNT_FOR_SUCCESS', 3000), // 90 days
+    'voting_days' => env('PETITION_VOTING_DAYS', 90) * 86400, // 90 days
+    'votes_count_for_success' => env('PETITION_VOTES_COUNT_FOR_SUCCESS', 300), // 300 votes
 
     'minutes_for_edit' => env('PETITION_MINUTES_FOR_EDIT', 60),
     'comment_minutes_for_edit' => env('PETITION_COMMENT_MINUTES_FOR_EDIT', 15),
